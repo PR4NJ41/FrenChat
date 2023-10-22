893a6cc85a99aed7d485a2f189d7500ecb3849f4
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2023-10-22 17:14:44 +0530

    Updated the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import "./App.css";
+import { lazy, Suspense } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import UserProvider from "./context/UserProvider";
+import AccountProvider from "./context/AccountProvider";
+import Loader from "./components/loader/Loader.js";
+
+const Messenger = lazy(() => import("./components/Messenger.js"));
 
 function App() {
+	const clientId = "1079104901870-nb75dqm9tppmji26ro4l126v8ld1gb3t.apps.googleusercontent.com";
+
 	return (
-		<div className="App">
-			<Messenger />
-		</div>
+		<GoogleOAuthProvider clientId={clientId}>
+			<UserProvider>
+				<AccountProvider>
+					<Suspense fallback={<Loader />}>
+						<Messenger />
+					</Suspense>
+				</AccountProvider>
+			</UserProvider>
+		</GoogleOAuthProvider>
 	);
 }
 
